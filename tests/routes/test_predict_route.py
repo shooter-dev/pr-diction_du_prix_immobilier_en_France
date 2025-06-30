ddc81476b80a5f9915706d87b6b833d9d91c5d0f
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2025-06-30 09:41:13 +0200

    Feat create route predict (#3)
    * feat: add uvicorn, fastAPI to requirements.txt
    
    * feat: update models
    
    * feat: update models
    
    * feat: remove models
    
    * feat: update models
    
    * feat: update predict
    
    * feat: update predict
    
    * feat: update test

--- tests/routes/test_predict_route.py
+++ tests/routes/test_predict_route.py
@@ -1,11 +1,17 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from app.schemas import OutputPredict
+
 
 client = TestClient(app)
 
 def test_post_ville():
-    ville = 'lille'
-    resp = client.post(f"/predict/{ville}")
+    body = {
+        "ville_name": "lille",
+        "surface_bati": 3000,
+        "nombre_pieces": 4,
+        "type_local": "Maison",
+        "surface_terrain": 400,
+        "nombre_lots": 10
+    }
+    resp = client.post(f"/predict", json=body)
     assert resp.status_code == 200
-    assert resp.json() == OutputPredict
